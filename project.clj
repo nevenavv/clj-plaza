b1ce331b73587f7b802116d2f82fcd8c57d6ba2f
Nevena <nevenavv@gmail.com>
2011-05-03 20:36:49 +0200

    clj version, removed autodoc dep (ant/lein version incopatibility)

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject plaza "0.0.5-SNAPSHOT"
   :description "Plaza framework for semantic distributed applications"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.1"]
+                 [org.clojure/clojure-contrib "1.2.0"]
                  [com.hp.hpl.jena/jena "2.6.2"]
                  [com.hp.hpl.jena/arq "2.8.3"]
                  [net.rootdev/java-rdfa "0.3"]
@@ -11,14 +11,14 @@
                  [nu.validator.htmlparser/htmlparser "1.2.0"]
                  [com.franz/openrdf-sesame-onejar "2.2"]
                  [commons-pool "1.5.4"]
-                 [org.jboss.netty/netty       "3.2.0.BETA1"]
+                 [org.jboss.netty/netty "3.2.0.BETA1"]
                  [com.rabbitmq/amqp-client "1.7.2"]
                  [log4j/log4j "1.2.14"]]
   :repositories { "jboss" "http://repository.jboss.org/nexus/content/groups/public/" }
   :dev-dependencies [[leiningen/lein-swank "1.2.0-SNAPSHOT"]
                      [clojure-http-client "1.1.0-SNAPSHOT"]
-                     [autodoc "0.7.1"]
-                     [lein-clojars         "0.5.0-SNAPSHOT"]]
+                     ;[autodoc "0.7.1"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]]
   :main     plaza.core
   :autodoc {:name "clj-plaza", :page-title "clj-plaza distributed semantic systems library"
             :author "Antonio Garrote <antoniogarrote@gmail.com> <agarrote@usal.es>"
